beaa1edd229b61205d4384185c8a866837d01a73
RAKSHITH B S <64544259+rkashyap621@users.noreply.github.com>
2026-06-06 20:20:20 +0530

    Create mdl1_longitudinal_vehicle_init_params.m

---
+++ scripts/mdl1_longitudinal_vehicle_init_params.m
@@ -0,0 +1,16 @@
+% Vehicle Parameters
+m   = 1200;      % kg
+C_r  = 0.015;
+rho = 1.225;     % kg/m^3
+C_d  = 0.32;
+A   = 2.2;       % m^2
+g   = 9.81;      % m/s^2
+
+% Input traction force
+F_T = 3000;      % N
+
+% Velocity
+v = 20;          % m/s
+
+% Flat road
+theta = 0;       % radians
